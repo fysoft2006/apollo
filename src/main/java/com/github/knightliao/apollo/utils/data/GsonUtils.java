6b6bf5da7c32faf9ef006b0c0ae6d1289d2951f0
x240pc <knightliao@gmail.com>
2014-08-20 23:34:22 +0800

    init

---
+++ src/main/java/com/github/knightliao/apollo/utils/data/GsonUtils.java
@@ -0,0 +1,24 @@
+package com.github.knightliao.apollo.utils.data;
+
+import com.google.gson.Gson;
+
+/**
+ * Google Json工具
+ * 
+ * @author liaoqiqi
+ * @version 2014-6-16
+ */
+public class GsonUtils {
+
+    /**
+     * 
+     * @param object
+     * @return
+     */
+    public static String toJson(Object object) {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        return json;
+    }
+}
---
+++ src/main/java/com/github/knightliao/apollo/utils/tool/TokenUtil.java
@@ -0,0 +1,44 @@
+package com.github.knightliao.apollo.utils.tool;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author liaoqiqi
+ * @version 2014-8-20
+ */
+public abstract class TokenUtil {
+
+    public static final String getTokenId(int length) {
+        if (0 == length) {
+            return "";
+        }
+
+        Date tt = new Date();
+        String mTimeStr = "" + tt.getTime();
+        int realLength = mTimeStr.length();
+        if (realLength >= length) {
+            return mTimeStr.substring(0, length);
+        }
+
+        String tempString = "";
+        String randomString = mTimeStr;
+
+        while (realLength < length) {
+            tempString = "" + Math.random();
+            randomString += tempString.substring(2);
+            realLength = randomString.length();
+        }
+
+        return randomString.substring(0, length);
+    }
+
+    /**
+     * 获取UUID
+     * 
+     * @return
+     */
+    public static final String generateToken() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
